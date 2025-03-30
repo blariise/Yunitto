87a12b913ceeb072b17b0f704a4b840aa6bce83b
blariise <61951559+blariise@users.noreply.github.com>
2025-03-30 23:13:14 +0000

    asset class fix

--- include/asset.h
+++ include/asset.h
@@ -3,15 +3,21 @@
 
 class Asset {
 public:
-  Asset();
-  virtual ~Asset() = default;
+  Asset() = delete;
+  Asset(std::string_view name, std::string_view ticker, std::string_view type, double currency);
 
+  std::string& getName() const;
+  std::string& getTicker() const;
+  std::string& getType() const;
+  double getCurrentPrice() const;
+  double getCurrency() const;
 private:
   std::string m_name;
   std::string m_ticker;
+  std::string m_type;
+  // transactions vector
   double m_current_price;
-  double m_current_value;
-  double quanity;
+  double m_currency;
 };
 #endif // ASSET_H
 
