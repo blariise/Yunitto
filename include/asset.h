691becc2fa173552ef4b0c651c2ff886b1139527
blariise <61951559+blariise@users.noreply.github.com>
2025-03-31 01:05:22 +0000

    indentation fix

--- include/asset.h
+++ include/asset.h
@@ -5,22 +5,22 @@
 #include <string_view>
 
 class Asset {
-public:
-  Asset() = delete;
-  Asset(std::string_view name, std::string_view ticker, std::string_view type, std::string_view currency);
+  public:
+    Asset() = delete;
+    Asset(std::string_view name, std::string_view ticker, std::string_view type, std::string_view currency);
 
-  const std::string& getName() const;
-  const std::string& getTicker() const;
-  const std::string& getType() const;
-  const std::string& getCurrency() const;
-  double getCurrentPrice() const;
-private:
-  std::string m_name;
-  std::string m_ticker;
-  std::string m_type;
-  std::string m_currency;
-  // transactions vector
-  double m_current_price;
+    const std::string& getName() const;
+    const std::string& getTicker() const;
+    const std::string& getType() const;
+    const std::string& getCurrency() const;
+    double getCurrentPrice() const;
+  private:
+    std::string m_name;
+    std::string m_ticker;
+    std::string m_type;
+    std::string m_currency;
+    // transactions vector
+    double m_current_price;
  
 };
 #endif // ASSET_H
