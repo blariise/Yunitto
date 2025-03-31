673c311175b2c37b8dc6ca25c1d1c66e55123c3a
blarrise <61951559+blariise@users.noreply.github.com>
2025-04-01 00:35:01 +0200

    add asset functionality

--- src/asset.cpp
+++ src/asset.cpp
@@ -12,7 +12,22 @@ const std::string& Asset::getType() const { return m_type; }
 const std::string& Asset::getCurrency() const { return m_currency; }
 double Asset::getCurrentPrice() const { return m_current_price; }
 double Asset::getTotalQuantity() const { return m_total_quantity; }                                      
+double Asset::getTotalCost() const { return m_total_cost; }
+double Asset::getCurrentValue() const { return m_current_value; }
+double Asset::getProfit() const { return m_profit; }
+
+void Asset::calculateProfit() {
+  m_profit = m_current_value - m_total_cost;
+}
+
+void Asset::calculateCurrentValue() {
+  m_current_value += (m_total_quantity * m_current_price);
+}
 
 void Asset::addTransaction(const Transaction& transaction) {
+  m_total_cost += (transaction.getQuantity() * transaction.getPrice());
+  m_total_quantity += transaction.getQuantity();
+  calculateCurrentValue();
+  calculateProfit();
   m_transactions.push_back(transaction);
 }
