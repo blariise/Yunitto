ad955b374a18aa3a6ed9f31bf319a64d2d7e2c16
blarrise <61951559+blariise@users.noreply.github.com>
2025-04-01 04:44:13 +0200

    fix asset and transaction classes

--- src/asset.cpp
+++ src/asset.cpp
@@ -11,23 +11,29 @@ const std::string& Asset::getTicker() const { return m_ticker; }
 const std::string& Asset::getType() const { return m_type; }
 const std::string& Asset::getCurrency() const { return m_currency; }
 double Asset::getCurrentPrice() const { return m_current_price; }
-double Asset::getTotalQuantity() const { return m_total_quantity; }                                      
-double Asset::getTotalCost() const { return m_total_cost; }
-double Asset::getCurrentValue() const { return m_current_value; }
-double Asset::getProfit() const { return m_profit; }
 
-void Asset::calculateProfit() {
-  m_profit = m_current_value - m_total_cost;
+double Asset::getQuantity() const { 
+  double quantity { 0 };
+  for (const auto& t : m_transactions)
+    quantity += t.getQuantity();
+  return quantity;
+}                 
+
+double Asset::getTotalCost() const {
+  double total_cost { 0 };
+  for (const auto& t : m_transactions)
+      total_cost += t.getTotalCost();
+  return total_cost;
 }
 
-void Asset::calculateCurrentValue() {
-  m_current_value += (m_total_quantity * m_current_price);
+double Asset::getCurrentValue() const {
+  return getQuantity() * m_current_price;
+}
+
+double Asset::getProfit() const {
+   return getCurrentValue() - m_current_price; 
 }
 
 void Asset::addTransaction(const Transaction& transaction) {
-  m_total_cost += (transaction.getQuantity() * transaction.getPrice());
-  m_total_quantity += transaction.getQuantity();
-  calculateCurrentValue();
-  calculateProfit();
   m_transactions.push_back(transaction);
 }
--- src/transaction.cpp
+++ src/transaction.cpp
@@ -7,6 +7,7 @@ Transaction::Transaction(double quantity, double price, std::string_view payment
   , m_price { price }
   , m_payment_currency { payment_currency }
   , m_date { date }
+  , m_total_cost { m_quantity * m_price }
   , m_transaction_id { s_id_generator++ } {}
 
 int Transaction::getTransactionId() const { return m_transaction_id; }
@@ -14,3 +15,4 @@ double Transaction::getQuantity() const { return m_quantity; }
 double Transaction::getPrice() const { return m_price; }
 const std::string& Transaction::getPaymentCurrency() const { return m_payment_currency; }
 const Date Transaction::getDate() const { return m_date; }
+double Transaction::getTotalCost() const { return m_total_cost; }
