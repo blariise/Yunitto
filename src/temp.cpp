aa1cc36b652199a9867e173561710c6c6d4e37ca
blarrise <61951559+blariise@users.noreply.github.com>
2025-04-01 06:08:10 +0200

    add file to test code

--- src/temp.cpp
+++ src/temp.cpp
@@ -1,4 +1,39 @@
-int main() {
+#include <iostream>
+
+#include "portfolio.h"
+#include "asset.h"
+#include "transaction.h"
+
+int main() { 
+
+  Portfolio crypto { "crypto" };
+
+  auto btc { std::make_shared<Asset>("Bitcoin", "BTC", "Crypto", "PLN")};
+ 
+  crypto.addAsset(btc);
+
+  const std::chrono::time_point now{std::chrono::system_clock::now()};
+  const std::chrono::year_month_day ymd{std::chrono::floor<std::chrono::days>(now)};
+
+  Transaction x {10, 100, "PLN", ymd};
+ 
+  std::cout << btc->getName() << " has: " << btc->getQuantity() << " quantity\n";
+
+  btc->addTransaction(x);
+
+  std::cout << btc->getName() << " has: " << btc->getQuantity() << " quantity\n";
+
+  std::cout << crypto.getName() << '\n';
+  std::cout << crypto.getTotalValue() << '\n';
+  
+  auto eth { std::make_shared<Asset>("Ethereum", "ETH", "Crypto", "PLN") };
+
+  crypto.addAsset(eth);
+
+  eth->addTransaction( {10, 30, "PLN", ymd} );
+
+  std::cout << crypto.getTotalValue() << '\n';
+
   return 0;
 }
 
