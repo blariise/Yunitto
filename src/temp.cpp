6c4d86b0671107960c8f2cb5e0e1ffa838e450ca
blarrise <61951559+blariise@users.noreply.github.com>
2025-04-01 22:11:59 +0200

    test portfolio-manager class

--- src/temp.cpp
+++ src/temp.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
+#include "portfoliomanager.h"
 #include "portfolio.h"
 #include "asset.h"
 #include "transaction.h"
 
 int main() { 
 
-  Portfolio crypto { "crypto" };
-
+  PortfolioManager pm {};
+  auto crypto { std::make_shared<Portfolio>("crypto") };
+  pm.addPortfolio(crypto);
   auto btc { std::make_shared<Asset>("Bitcoin", "BTC", "Crypto", "PLN")};
- 
-  crypto.addAsset(btc);
+  crypto->addAsset(btc);
 
   const std::chrono::time_point now{std::chrono::system_clock::now()};
   const std::chrono::year_month_day ymd{std::chrono::floor<std::chrono::days>(now)};
@@ -23,17 +24,16 @@ int main() {
 
   std::cout << btc->getName() << " has: " << btc->getQuantity() << " quantity\n";
 
-  std::cout << crypto.getName() << '\n';
-  std::cout << crypto.getTotalValue() << '\n';
+  std::cout << crypto->getName() << '\n';
+  std::cout << crypto->getTotalValue() << '\n';
   
   auto eth { std::make_shared<Asset>("Ethereum", "ETH", "Crypto", "PLN") };
 
-  crypto.addAsset(eth);
+  crypto->addAsset(eth);
 
   eth->addTransaction( {10, 30, "PLN", ymd} );
 
-  std::cout << crypto.getTotalValue() << '\n';
-
+  std::cout << crypto->getTotalValue() << '\n';
   return 0;
 }
 
