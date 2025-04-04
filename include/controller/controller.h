48c153790ca75487750d7b5531132b3bb93633d5
blarrise <61951559+blariise@users.noreply.github.com>
2025-04-04 09:28:11 +0200

    add add/remove print assets from controller

--- include/controller/controller.h
+++ include/controller/controller.h
@@ -21,6 +21,8 @@ class Controller {
         std::string_view ticker,
         std::string_view type,
         std::string_view currency);
+    void removeAsset(std::size_t portfolio_index,std::size_t asset_index);
+    void printAssets(std::size_t portfolio_index) const;
 
   private:
     std::unique_ptr<PortfolioManager> m_portfolio_manager;
