d100af947b096acafb975972ed326511e4370df8
blarrise <61951559+blariise@users.noreply.github.com>
2025-04-20 10:48:06 +0200

    cleanup

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -3,29 +3,24 @@
 #include <QQmlContext>
 
 #include "controllerwrapper.h"
-#include "controller.h"  // Assuming this is your main controller class
+#include "controller.h"
 
 #include <QDirIterator>
 #include <QDebug>
 #include <QUrl>
 
-void listQRCResources(const QString& path = QStringLiteral(":/")) {
-  QDirIterator it(path, QDirIterator::Subdirectories);
-  while (it.hasNext())
-    qDebug() << "QRC file:" << it.next();
-}
-
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
 
-  // Create Controller instance and wrap it
+  const std::chrono::time_point now{std::chrono::system_clock::now()};
+  const std::chrono::year_month_day ymd{std::chrono::floor<std::chrono::days>(now)};
+
   auto portfolio_manager { std::make_unique<PortfolioManager>() };
   auto controller { std::make_unique<Controller>(std::move(portfolio_manager)) };
   ControllerWrapper controllerWrapper { std::move(controller) };
 
   QQmlApplicationEngine engine;
   engine.rootContext()->setContextProperty("controller", &controllerWrapper);
-  listQRCResources();
   engine.load(QUrl(QStringLiteral("qrc:resources/qml/main.qml")));
   return app.exec();
 }
